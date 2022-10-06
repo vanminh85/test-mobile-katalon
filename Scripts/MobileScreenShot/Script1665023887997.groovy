daa2050bbe26f50a26f2f39a454a28f81c87b0e5
vanminh85 <minh.nguyen@katalon.com>
2022-10-06 16:02:26 +0700

    commit

---
+++ Scripts/MobileScreenShot/Script1665023887997.groovy
@@ -0,0 +1,16 @@
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.util.internal.PathUtil as PathUtil
+
+'Start application on current selected android\'s device'
+Mobile.startApplication(GlobalVariable.G_AndroidApp, false)
+
+'Take screenshot of current device screen'
+Mobile.takeScreenshotAsCheckpoint('screenshot')
+
+'Close application on current selected android\'s device'
+Mobile.closeApplication()
